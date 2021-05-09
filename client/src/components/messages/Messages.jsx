00bfebb338d369f86f423a51f64817b306bb9abc
Tanuj <tanujupadhyay145.t.u@gmail.com>
2021-05-09 18:32:46 +0530

    adding notification

--- client/src/components/messages/Messages.jsx
+++ client/src/components/messages/Messages.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import ScrollToBottom from 'react-scroll-to-bottom';
+import iphoneNot from '../sound/Iphone.mp3';
 
 import Message from './message/Message';
 
@@ -8,7 +9,20 @@ import './messages.css';
 
 const Messages = ({ messages, name }) => (
     <ScrollToBottom className="messages">
-        {messages.map((message, i) => <div key={i}><Message message={message} name={name} /></div>)}
+        {messages.map((message, i) => <div key={i}><Message message={message} name={name} />
+            {
+                message && (
+                    <audio
+                        autoPlay="autoPlay"
+                        href="audio_tag"
+
+                        src={iphoneNot}
+                        type="audio/mpeg"
+                        hidden
+                    ></audio>
+                )
+            }
+        </div>)}
     </ScrollToBottom>
 );
 
--- client/src/components/textContainer/TextContainer.jsx
+++ client/src/components/textContainer/TextContainer.jsx
@@ -8,6 +8,7 @@ const TextContainer = ({ users }) => (
     <div className="textContainer">
         <div>
             <h1>Realtime Chat Application <span role="img" aria-label="emoji">💬</span></h1>
+            <h3>Develop by Tanuj Upadhyay<span role="img" aria-label="emoji">💬</span></h3>
             <h2>Created with React, Express, Node and Socket.IO <span role="img" aria-label="emoji">❤️</span></h2>
             <h2>Try it out right now! <span role="img" aria-label="emoji">⬅️</span></h2>
         </div>
