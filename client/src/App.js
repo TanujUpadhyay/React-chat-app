7fd0ebb921fd924a8ac269cb8e3b1eddce6bac1b
Tanuj <tanujupadhyay145.t.u@gmail.com>
2021-05-09 16:04:41 +0530

    #2 setup basic backen server which is working

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from "react-router-dom"
 
 import Join from "./components/Join"
-import Chat from "./components/chat"
+import Chat from "./components/Chat"
 
 function App() {
     return (
